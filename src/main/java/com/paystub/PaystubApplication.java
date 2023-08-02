7b71c59f5e0400abb06e07aa540db4c80b81f0c8
toychip <dlawnsgud427@naver.com>
2023-08-02 13:48:10 +0900

    Feat: Paystub 초안(#main)

---
+++ src/main/java/com/paystub/PaystubApplication.java
@@ -0,0 +1,13 @@
+package com.paystub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaystubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaystubApplication.class, args);
+	}
+
+}
