7087e7a76392504f5a0fbe537854df3faf933bc4
seonu1204 <seonu7222@daum.net>
2023-08-24 09:12:36 +0900

    De: 로그아웃 alert 선택 옵션 추가 (#48)

--- src/main/java/com/paystub/controller/AuthController.java
+++ src/main/java/com/paystub/controller/AuthController.java
@@ -4,11 +4,16 @@ import com.paystub.dto.request.LoginRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,11 +21,13 @@ public class AuthController {
 
     // 로그인
     @GetMapping("/login")
-    public String loginForm(Model model) {
+    public String loginForm(@Validated Model model) {
 
         // 아이디와 비밀번호를 입력할 수 있는 빈껍데기를 담아서 모델에 담음
         model.addAttribute("loginDto", new LoginRequest());
+
         return "loginForm";
+
     }
 
     @GetMapping("/logout")
